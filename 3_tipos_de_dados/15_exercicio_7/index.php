5c2d5ae2792f936622951d83d1b1d4f89f28f752
filipe augusto <f04a07s19c93@gmail.com>
2024-02-21 19:50:53 -0300

    obre array

--- 3_tipos_de_dados/15_exercicio_7/index.php
+++ 3_tipos_de_dados/15_exercicio_7/index.php
@@ -1,15 +1,25 @@
 <?php
 
-  $pessoa = [
-    'nome' => 'Matheus',
-    'idade' => 11,
-    'profissao' => 'Programador',
-    'graduacao' => 'Sistemas da Informação'
-  ];
+  $pessoa = ["nome"=>"filipe",
+  "peso" => 80,
+  "altura" =>1.78,
+  "imc"=> 0,
+  "sexo"=>"masculino"  
+];
 
-  $maioridade = 18;
+$pessoa["imc"] = $pessoa["peso"] /($pessoa["altura"] * $pessoa["altura"]);
 
-  // desafio
-  if($pessoa['idade'] >= $maioridade) {
-    echo "A pessoa é maior de idade!";
-  }
+$imc = $pessoa["imc"];
+$nome = $pessoa["nome"];
+
+echo "O IMC da pessoa $nome é de $imc";
+echo "<br>";
+
+if(is_float($imc)){
+echo "imc é float";
+echo "<br>";
+if($imc > 24){
+  echo "$nome esta acima do peso";
+}    
+
+}   
