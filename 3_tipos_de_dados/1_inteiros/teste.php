1ab143b3b6d0d9a2a5aa021167ee1582a2131d5f
filipe augusto <f04a07s19c93@gmail.com>
2024-02-21 17:31:20 -0300

    numeros inteiros

---
+++ 3_tipos_de_dados/1_inteiros/teste.php
@@ -0,0 +1,12 @@
+<?php
+  echo "soma 5: ";
+  echo 5+5;
+  echo "</br>subtracao 5: ";
+  echo 5-5;
+  echo "</br>multiplicacao 5: ";
+  echo 5*5;
+  echo "</br>divisao 5: ";
+  echo 5/5;
+  echo "</br> soma 5 + 5 = ".(5 + 5) ;
+
+  echo $n;
