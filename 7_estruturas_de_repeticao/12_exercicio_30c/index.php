bf513738390a2a9acac6a6a5a701d23b56196670
f04a07s19c93 <f04a07s19c93@gmail.com>
2024-03-13 10:07:46 -0300

    loop

--- 7_estruturas_de_repeticao/12_exercicio_30c/index.php
+++ 7_estruturas_de_repeticao/12_exercicio_30c/index.php
@@ -1,18 +1,57 @@
 <?php
 
-  $arr = [];
+$array = [];
 
-  for($i = 10; $i <= 20; $i++) {
-    array_push($arr, $i);
+for ($i = 10; $i <= 20; $i++) {
+array_push($array, $i);
+}
+for ($x = 0; $x <= count($array); $x++) {
+echo $array[$x];
+echo "</br>";
+}
+
+for( $i = 0; $i <= count($array); $i++){
+
+  if ($array[$i]% 2 > 0) {
+    echo " Número ".$array[$i]."é impar" ;
+    echo "</br>";
   }
+}
 
-  print_r($arr);
-  echo "<br>";
 
-  for($i = 0; $i < count($arr); $i++) {
 
-    if($arr[$i] % 2 != 0) {
-      echo "Número ímpar: $arr[$i] <br>";
-    }
 
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // $arr = [];
+
+  // for($i = 10; $i <= 20; $i++) {
+  //   array_push($arr, $i);
+  // }
+
+  // print_r($arr);
+  // echo "<br>";
+
+  // for($i = 0; $i < count($arr); $i++) {
+
+  //   if($arr[$i] % 2 != 0) {
+  //     echo "Número ímpar: $arr[$i] <br>";
+  //   }
+
+  //}
